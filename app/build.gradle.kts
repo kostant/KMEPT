656cdb3f539dc5655e4c20a0b2f4e0840047344a
Студент <student@imes.local>
2024-05-17 17:32:41 +0300

    add Antonchik Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.kmept.kmept"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.kmept.kmept"
--- app/src/main/java/ru/kmept/kmept/MainActivity.kt
+++ app/src/main/java/ru/kmept/kmept/MainActivity.kt
@@ -1,11 +1,19 @@
 package ru.kmept.kmept
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.kmept.kmept.antonchik.AntonchikActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onAntonchikClick(view: View) {
+        val intent = Intent(MainActivity@this, AntonchikActivity::class.java)
+        startActivity(intent)
     }
 }
